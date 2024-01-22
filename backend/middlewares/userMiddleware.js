867776d0c2d7c268079485c66a2a2e7867e4aff4
Prathyarthi <prathyarti@gmail.com>
2024-01-22 16:22:33 +0530

    Modified middleware

--- backend/middlewares/userMiddleware.js
+++ backend/middlewares/userMiddleware.js
@@ -1,23 +1,31 @@
 import jwt from "jsonwebtoken";
-require('dotenv/config')
+import { config } from "dotenv"
+config()
 
 const authMiddleware = (req, res, next) => {
     const authHeader = req.headers.authorization;
 
     if (!authHeader || !authHeader.startsWith('Bearer ')) {
-        return res.status(403).json({});
+        return res.status(403).json({
+            success: false,
+            message: "Invalid Header"
+        });
     }
 
     const token = authHeader.split(' ')[1];
 
     try {
         const decoded = jwt.verify(token, JWT_SECRET);
+        if (decoded.userId) {
+            req.userId = decoded.userId;
+            next();
+        }
 
-        req.userId = decoded.userId;
-
-        next();
     } catch (err) {
-        return res.status(403).json({});
+        return res.status(403).json({
+            success: false,
+            message: "Could not verify the token"
+        });
     }
 };
 
