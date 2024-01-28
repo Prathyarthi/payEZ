d139947af090cb79f10e2bb64c26eeb7e431cbc6
Prathyarthi <prathyarti@gmail.com>
2024-01-28 08:55:44 +0530

    Done with components

--- frontend/src/components/Input_box.jsx
+++ frontend/src/components/Input_box.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-function Input_box({ label, placeholder }) {
+function Input_box({ label, placeholder, onChange }) {
     return (
         <div>
             <div className='text-sm font-medium text-left py-2'>
                 {label}
             </div>
-            <input type="text" placeholder={placeholder} className='w-full p-2 border rounded boder-slate-200' />
+            <input onChange={onChange} type="text" placeholder={placeholder} className='w-full p-2 border rounded boder-slate-200' />
         </div>
     )
 }
--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Heading from '../components/Heading'
+import Sub_Heading from '../components/Sub_Heading'
+import Input_box from '../components/Input_box'
+import Button from '../components/Button'
+import Bottom_warning from '../components/Bottom_warning'
 
 function Signin() {
   return (
-    <div>Signin</div>
+    <div className="bg-slate-300 h-screen flex justify-center">
+      <div className="flex flex-col justify-center">
+        <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
+          <Heading label={"Sign in"} />
+          <Sub_Heading label={"Enter your credentials to access your account"} />
+          <Input_box placeholder="Ex: john@gmail.com" label={"Email"} />
+          <Input_box placeholder="Ex: John@123#" label={"Password"} />
+          <div className="pt-4">
+            <Button label={"Sign in"} />
+          </div>
+          <Bottom_warning label={"Don't have an account?"} buttonText={"Sign up"} to={"/signup"} />
+        </div>
+      </div>
+    </div>
   )
 }
 
